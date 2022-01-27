343ac7cfdfe62cdca1ead660ab7ba7cc7bcd578c
Fahrilahmad <96267756+Fahrilahmad@users.noreply.github.com>
2022-01-27 13:42:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['085876902820'] // Put your number here //owner eval
-global.kontak = ['085876902820','0'] //Ketika ada yang ngetik #owner
+global.owner = ['6285876902820'] // Put your number here //owner eval
+global.kontak = ['6285876902820','0'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['085876902820'] // Premium user has unlimited limit
+global.prems = ['6285876902820'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
