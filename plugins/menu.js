e22679cf30606df029609a2208419a56f989c7ef
Fahrilahmad <96267756+Fahrilahmad@users.noreply.github.com>
2022-01-29 09:22:58 +0700

    Create menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -178,7 +178,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let names = m.fromMe ? conn.user : conn.contacts[who]
     let pushname = `${names.vnmae || names.notify || names.names || ('+' + names.jid.split`@`[0])}`
-    let pushn = 'Daftar Dulu ya kak supaya namanya muncul disini'
+    let pushn = 'menampilkan MENU fahrilbotz'
     let name = registered ? global.db.data.users[m.sender].name : pushn
     let d = new Date(new Date + 3600000)
     let locale = 'id'
