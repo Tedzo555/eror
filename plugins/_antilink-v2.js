362285836b01bc55ad7db03cd613fa42f65f168d
Fahrilahmad <96267756+Fahrilahmad@users.noreply.github.com>
2022-01-30 06:11:27 +0700

    Update _antilink-v2.js

--- plugins/_antilink-v2.js
+++ plugins/_antilink-v2.js
@@ -10,7 +10,7 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
   if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
     let thisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
     if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
-    await conn.sendButton(m.chat, `*Virtext Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *#on restrict* supaya bisa kick'}`, '©RFK Bot', 'Matikan Antilink', '#off antilink', m)
+    await conn.sendButton(m.chat, `*Virtext Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *#on restrict* supaya bisa kick'}`, '©fahril Bot', 'Matikan Antilink', '#off antilink', m)
     //if (global.opts['restrict']) {
       if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
 //    }
