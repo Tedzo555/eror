2aedff81332e1c8304fe28a66ae7f81124157023
Fahrilahmad <96267756+Fahrilahmad@users.noreply.github.com>
2022-01-30 14:10:10 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
 m.reply(wait)
-let res = await fetch(`https://rset-apikey.herokuapp.com/api/tiktod/?Apikey=${lolkey}&url=${args[0]}`)
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
@@ -15,11 +15,16 @@ await conn.sendFile(m.chat, link, 'tt.mp4', `
 - *By:* ${author.nickname} (${author.username})
 - *Desc:*
 ${description}
-`.trim(), m)
+`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 }
 
 handler.help = ['tiktok <url>']
 handler.tags = ['downloader']
-handler.command = /^tiktok$/i
-
+//handler.command = /^tiktok$/i
+handler.command = /^(tik(tok)?(dl)?)$/i
+handler.limit = true
 module.exports = handler
+
+
+//await conn.sendFile(m.chat, url, 'tiktok.mp4', kasihcaption, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+
