2520ad7a22fe94488ee0a0f6201237eecb8f4bf3
Fahrilahmad <96267756+Fahrilahmad@users.noreply.github.com>
2022-01-31 19:06:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 085876902820
-├ DANA : 085876902820
+├ PULSA : 6285876902820
+├ DANA  : 6285876902820
+├ saweria : https://saweria.co/Fahrilgg
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
@@ -10,3 +11,4 @@ handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
 
 module.exports = handler
+
