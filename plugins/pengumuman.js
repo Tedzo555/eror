b35bc044a55ce82f49c0b518c25bb8413d540d7d
Fahrilahmad <96267756+Fahrilahmad@users.noreply.github.com>
2022-02-03 09:17:14 +0700

    Update pengumuman.js

--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -1,34 +1,37 @@
 const { MessageType } = require('@adiwajshing/baileys')
 
-let handler = async (m, { conn, text, participants }) => {
-  let users = participants.map(u => u.jid)
+let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
+  if (m.isGroup) {
+    if (!(isAdmin || isOwner)) {
+      dfail('admin', m, conn)
+      throw false
+    }
+  }
+  let users = participants.map(u => u.jid).filter(u => u != conn.user.jid)
   let q = m.quoted ? m.quoted : m
   let c = m.quoted ? m.quoted : m.msg
   let msg = conn.cMod(
     m.chat,
     conn.prepareMessageFromContent(
       m.chat,
-      { [c.toJSON ? q.mtype : MessageType.extendedText]: c.toJSON ? c.toJSON() : {
-        text: c || ''
-      } },
+      {
+        [c.toJSON ? q.mtype : MessageType.extendedText]: c.toJSON ? c.toJSON() : {
+          text: c || ''
+        }
+      },
       {
         contextInfo: {
           mentionedJid: users
-        },
-        quoted: m
+        }
       }
     ),
-    text || q.text 
+    text || q.text
   )
   await conn.relayWAMessage(msg)
 }
-handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => v + ' [teks]')
+handler.help = ['hidetag'].map(v => v + ' [teks]')
 handler.tags = ['group']
-handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
-
-handler.group = true
-handler.admin = true
-handler.owner = true
+handler.command = /^(h(ide)?tag)$/i
 
 module.exports = handler
 
